cfabcd18c67b63991aa5fcdc8dc2311afda281fb
Bie Yaqing <bieyaqing@gmail.com>
2017-05-25 13:50:40 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+// 'use strict';
+
+// var http = require('http');
+// var routes = require('./app/routes');
+
+// http.createServer(function (req, res) {
+// 	routes.invoke(req, res);
+// }).listen(8080);
+
 'use strict';
 
-var http = require('http');
+const express = require('express');
+
+const app = express();
+
 var routes = require('./app/routes');
 
-http.createServer(function (req, res) {
+app.get('/', (req, res) => {
 	routes.invoke(req, res);
-}).listen(8080);
+});
+
+if (module === require.main) {
+	const server = app.listen(process.env.PORT || 8081, () => {
+		const port = server.address().port;
+		console.log(`App listening on port ${port}`);
+	});
+}
+
+module.exports = app;
